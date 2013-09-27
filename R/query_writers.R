b543712d7c67406e2425f6911f950dee8c41560e
Sean Garborg <sean.garborg@gmail>
2013-09-27 18:04:15 -0500

    support more where clauses, hiding fields from views

--- R/query_writers.R
+++ R/query_writers.R
@@ -1,9 +1,16 @@
 #library(data.table)
 
+chrEscape = function(chr) {
+	if (length(chr)) {
+		paste0("'", gsub("'", "''", chr), "'")
+	} else
+		chr
+}
+
 enquote = function(name) {
-	if (length(name)) 
+	if (length(name)) {
 		paste0('"', name, '"')
-	else
+	} else
 		name
 }
 
@@ -26,19 +33,22 @@ indentWith = function(v, sep) {
 		paste0('  ', v, c(rep_len(sep, lv-1), ''))
 }
 
-getFields = function(name, wormhole=F) {
+getFields = function(name, combine=T, wormhole=F) {
 	if (substring(name, 1, 1) == '@') {
 		vs = viewSpec(name)
-		unlist(lapply(names(vs), function(n) {
-			all = getFields(n, wormhole=T)
-			hide = enquote(vs[[n]][['hide']])
+		fields = lapply(names(vs), function(name) {
+			all = getFields(name, wormhole=T)
+			hide = enquote(vs[[name]][['hide']])
 			all[!names(all) %chin% hide]
-		}))
+		})
+		if (combine || wormhole)
+			fields = unlist(fields)
+		fields
 	} else {
 		rf = enquoteNames(tableFields(name))
 		if (wormhole) {
 			rf = names(rf)
-			setattr(rf, 'names', rf)  
+			setattr(rf, 'names', rf)
 		}
 		rf
 	}
@@ -84,13 +94,12 @@ qBuild = function(select, from, where=NULL, aliased=NULL, wormhole=F) {
 			where = enquoteNames(where)
 	}
 	
-	view = (substr(from, 1, 1) == '@')
-	if (view) {
+	if (substr(from, 1, 1) == '@') {
 		if (is.null(aliased))
 			aliased = rep(F, 26)
 		
-		r = nest(select, viewSpec(from), where, aliased)
-		print('nested')
+		r = nest(select, from, where, aliased)
+
 	} else {
 		fields = getFields(from)
 		fnames= names(fields)
@@ -102,24 +111,8 @@ qBuild = function(select, from, where=NULL, aliased=NULL, wormhole=F) {
 		selects = fields[select]
 		select_strs = paste(selects, 'AS', names(selects))
 
-		if (!is.null(where)) {
-			wheres = mapply(
-				function(expr, crit) {
-					crit_str = switch(mode(crit),
-						character=paste0("'", paste(crit, collapse="', '"), "'"),
-						paste(crit, collapse=', '))
-
-					if (length(crit) == 1) {
-						paste(expr, '=', crit_str)
-					} else
-						paste0(expr, ' in (', crit_str, ')')
-				},
-				fields[names(where)],
-				where
-			)
-			wheres = c('WHERE', indentWith(wheres, ' AND'))
-		} else
-			wheres = NULL
+		wheres = if (length(where))
+			c('WHERE', indentWith(getWheres(where, fields), ' AND'))
 		
 		r = list(
 			q=c('SELECT',
@@ -130,7 +123,6 @@ qBuild = function(select, from, where=NULL, aliased=NULL, wormhole=F) {
 				),
 			a=aliased
 		)
-		print('unnested')
 	}
 	
 	if (wormhole) {
@@ -140,10 +132,59 @@ qBuild = function(select, from, where=NULL, aliased=NULL, wormhole=F) {
 	}
 }
 
-nest = function(select, view_spec, where, aliased) {
+getWheres = function(where, fields) {
+
+  wo = mapply(
+    function(expr, crit) {
+      if (is.list(crit)) {
+        op = crit[[1]]
+        crit = crit[[2]]
+      } else
+        op = '='
+      
+      un = substr(op, 1, 1) == '!'
+      if (un)
+      	op = sub('^![ ]*', '', op)
+
+      if (class(crit) == 'Date')
+        crit = as.character(crit)
+      if (class(crit) == 'character')
+        crit = chrEscape(crit)
+
+     	multi_crit = length(crit) > 1
+      if (is.null(crit)) {
+      	op = 'is'
+      	crit = 'NULL'
+      } else if (multi_crit) {
+	      crit = if (op == 'between') {
+	      		paste(crit, collapse=' and ')
+	    		} else
+	    			paste0('(', paste(crit, collapse=', '), ')')
+      }
+
+      if (op == '=' && multi_crit)
+      	op = 'in'
+      if (un) {
+      	op = if (op == '=') {
+      			'<>'
+      		} else
+      			paste('not', op)
+      }
+
+      paste(expr, op, crit)
+    },
+    fields[names(where)],
+    where
+  )
+  wo
+}
+
+nest = function(select, from, where, aliased) {
 	print('<NEST>')
+	view_spec = viewSpec(from)
 	names = names(view_spec)
-	fields = lapply(names, getFields, wormhole=T)
+	fields = getFields(from, combine=F)
+
 	seq = seq_along(fields)
 
 	if (length(where))
@@ -152,20 +193,24 @@ nest = function(select, view_spec, where, aliased) {
 	q = NULL
 	for (i in seq) {
 		fields_i = fields[[i]]
+		hide_i = enquote(view_spec[[i]][['hide']])
+		fields_i = fields_i[!names(fields_i) %chin% hide_i]
+
+
 		join_i = view_spec[[i]][['join']]
 		on_i = enquote(join_i[['on']])
 
 		#~ Divy selects and wheres
-		inter_i = intersect(select, fields_i)
-		select_i = union(on_i, inter_i)
+		select_i = union(on_i, intersect(select, fields_i))
+		other_i = setdiff(select_i, on_i)
 
 		where_i = if (length(where))
 			where[intersect(wnames, fields_i)]
 
 		#~ Join if you have to
 		lazy = isTRUE(join_i[['lazy']])
-	
-		if (!lazy || length(inter_i) || length(where_i)) {
+
+		if (!lazy || length(other_i) || length(where_i)) {
 
 			#~ Build query 'i'
 			qb = qBuild(select_i, names[i], where_i, aliased, wormhole=T)
@@ -224,120 +269,3 @@ join = function(select, type, on, q1, q2, a1, a2) {
 	)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# join = function(select, view_spec, where, aliased) {
-# 	vst = view_spec[['tables']]
-# 	vsj = view_spec[['join']]
-
-# 	type = toupper(vsj[['type']])
-# 	on = enquote(vsj[['on']])
-	
-# 	t1 = vst[1]
-# 	t2 = vst[2]
-# 	names1 = names(getFields(t1))
-# 	names2 = names(getFields(t2))
-	
-# 	joint = intersect(names1, names2)
-# 	if (!setequal(on, joint))
-# 		stop(sprintf("Overlapping names (%s) in '%s' and '%s' don't match key (%s)",
-# 								 paste(joint, collapse=', '),
-# 								 t1, t2,
-# 								 paste(on, collapse=', ')))
-	
-# 	# add handling
-	
-# 	select1 = union(on, intersect(select, names1))
-# 	select2 = union(on, intersect(select, names2))
-# 	print('joinwhere:')
-# 	print(where)
-# 	print('names1:')
-# 	print(names1)
-# 	print('names2:')
-# 	print(names2)
-# 	if (length(where)) {
-# 		wnames = names(where)
-# 		where1 = where[intersect(wnames, names1)]
-# 		where2 = where[intersect(wnames, names2)]
-# 	}
-# 	print('where1:')
-# 	print(where1)
-# 	print('where2:')
-# 	print(where2)
-# 	qa1 = qBuild(select1, t1, where1, aliased, wormhole=T)
-# 	q1 = qa1[['q']]
-# 	aliased = qa1[['a']]
-	
-# 	qa2 = qBuild(select2, t2, where2, aliased, wormhole=T)
-# 	q2 = qa2[['q']]
-# 	aliased = qa2[['a']]
-
-# 	n = sum(aliased)
-# 	if (n == 26)
-# 		stop('No letters left in alphabet. Perhaps a new table or two...')
-# 	a1 = letters[n + 1]
-# 	aliased[n + 1] = T
-	
-# 	n = sum(aliased)
-# 	if (n == 26)
-# 		stop('No letters left in alphabet. Perhaps a new table or two...')
-# 	a2 = letters[n + 1]
-# 	aliased[n + 1] = T
-	
-# 	for (s in intersect(on, select)) {
-# 		newsel = switch(type,
-# 										full=paste('case', tf(a1, s), 'when null then',
-# 															 tf(a2, s), 'else', tf(a1, s),  'end AS', s),
-# 										right=tf(a2, s),
-# 										tf(a1, s))
-		
-# 		select[match(s, select)] = newsel
-# 	}
-	
-# 	on = paste(tf(a1, on), '=', tf(a2, on))
-
-# 	q = c(
-# 		'SELECT',
-# 		indentWith(select, ','),
-# 		'FROM (',
-# 		indentWith(c(
-# 			indentWith(q1, ''),
-# 			paste(') AS', a1),
-# 			paste(type, "JOIN ("),
-# 			indentWith(q2, ''),
-# 			paste(') AS', a2, 'ON'),
-# 			indentWith(on, ' AND')
-# 		), '')
-# 	)
-	
-# 	return(list(q=q, a=aliased))
-# }
-
-# getFields = function(names, wormhole=F) {
-# 	fields = unlist(lapply(
-# 		names,
-# 		function(name) {
-# 			if (substring(name, 1, 1) == '@') {
-# 				getFields(viewSpec(name)[['tables']], wormhole=T)
-# 			} else {
-# 				rf = enquoteNames(tableFields(name))
-# 				if (wormhole) {
-# 					rf = names(rf)
-# 					setattr(rf, 'names', rf)          
-# 				}
-# 				rf
-# 			}
-# 		}))
-# }
